285b947eeec46427f5754337ea6c156fc0d26377
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-12-12 08:32:50 +0100

    .

--- alap_ellenorzes.js
+++ alap_ellenorzes.js
@@ -1,1 +1,14 @@
 // /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/
+
+function ellenorAlap()
+{
+    let nev = document.forms["alapUrlap"]["nev"].value;
+    console.log(nev);
+
+    if(!nev.trim() === ""){
+        alert("A név mező nem lehet üres!");
+        return false;
+    }
+    
+    let nevfilter = /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/;
+}
